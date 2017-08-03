32713b3ee55d2dd2c00eb5737b4535188ccf9fd2
Manish Goyal <mgoyal05.mg@gmail.com>
2017-08-03 18:34:14 +0530

    updated Assignment Day 5

---
+++ src/Assignment_Day5/RegexExample.java
@@ -0,0 +1,18 @@
+package Assignment_Day5;
+import java.util.regex.Pattern;
+
+/**
+ * @author Manish Goyal
+ * A class to illustrate use of Regular Expression
+ */
+public class RegexExample {
+    public static void main(String[] args) {
+
+        /*'[A-Z]' will ensure that first letter should be capital
+          '.' ensures that next character can be of any input
+          '*' Matches zero or more occurrences of this pattern.
+          '\\.' ensures that it should always end with period
+         */
+        System.out.println("A Sample sentenceto show application of regex.".matches("[A-Z].*\\."));
+    }
+}
